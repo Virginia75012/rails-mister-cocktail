52181c126d8aa1b6be2d3062fedcd6e858c00c2e
Virginia Atenza <virginia.atenza@gmail.com>
2020-02-14 12:54:22 +0100

    Add dotenv - protect .env

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Tequila Sunrise")
+Cocktail.create(name: "Irish Coffe")
+
